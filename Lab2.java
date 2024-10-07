ef8f68d79b7f67a77627118e9817a60bf1dcb45d
Kiril Yakymchuk <stbestich@gmail.com>
2024-10-07 18:54:00 +0300

    fixed compilation errors

--- Lab2.java
+++ Lab2.java
@@ -6,28 +6,27 @@ public class Lab2 {
     try {
       if (args.length < 2) {
         throw new IllegalArgumentException("Required arguments are missing.\nUsage: java Lab2 'text to sort' 'target letter' ");
-
-        String text = args[0];
-        char targetLetter = args[1].charAt(0);
-
-        String sortedText = sortTextByLetterOccurrences(text, targetLetter);
-
-        System.out.println(sortedText);
       }
+
+      String text = args[0];
+      char targetLetter = args[1].charAt(0);
+      String sortedText = sortTextByLetterOccurrences(text, targetLetter);
+
+      System.out.println(sortedText);
     } catch (Exception e) {
-      System.error.println(e.getMessage());
+      System.err.println(e.getMessage());
     }
   }
 
-  public String sortTextByLetterOccurrences(String text, char targetLetter) {
+  public static String sortTextByLetterOccurrences(String text, char targetLetter) {
     String[] words = text.split("\\s+");
     
-    Array.sort(words, (word1, word2) -> countLetter(word1, targetLetter) - countLetter(word2, targetLetter));
+    Arrays.sort(words, (word1, word2) -> countLetter(word1, targetLetter) - countLetter(word2, targetLetter));
     
     return String.join(" ", words); 
   }
 
-  public int countLetter(String word, char targetLetter) {
+  public static int countLetter(String word, char targetLetter) {
     int count = 0;
     for (char letter : word.toCharArray()) {
       if (letter == targetLetter) {
